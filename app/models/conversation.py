9afb565c0759e6a2f9b760030653c75ea8cc469e
Dan-Ale-Es-A <alexandraarenas1705@gmail.com>
2024-06-01 00:40:36 -0500

    feat:add_class_conversation

---
+++ app/models/conversation.py
@@ -0,0 +1,11 @@
+class Conversation(db.Model):
+    __tablename__ = 'conversations'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    message = db.Column(db.Text, nullable=False)
+    response = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+    user = db.relationship('User', back_populates='conversations')
+
+User.conversations = db.relationship('Conversation', order_by=Conversation.timestamp, back_populates='user')
